aca0f5251132cc6138a8f9a27126f87043fb517b
Brad Phillips <brad@bradphillips.org>
2024-04-19 07:02:06 -0500

    Initial commit.

---
+++ EverFlow/EverFlow.h
@@ -0,0 +1,18 @@
+//
+//  EverFlow.h
+//  EverFlow
+//
+//  Created by Bradley Phillips on 4/19/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EverFlow.
+FOUNDATION_EXPORT double EverFlowVersionNumber;
+
+//! Project version string for EverFlow.
+FOUNDATION_EXPORT const unsigned char EverFlowVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EverFlow/PublicHeader.h>
+
+
